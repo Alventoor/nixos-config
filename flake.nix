79da98a1a5ce5e26c0283f9be0d42b5b5c612004
alventoor <julienm99@tutamail.com>
2023-01-24 13:56:15 +0100

    add a new system configuration - quark

--- flake.nix
+++ flake.nix
@@ -1,8 +1,15 @@
 {
-  inputs.sops-nix.url = github:Mic92/sops-nix;
-  inputs.nixos-hardware.url = github:NixOS/nixos-hardware/master;
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
 
-  outputs = { self, nixpkgs, sops-nix, nixos-hardware }: {
+    sops-nix.url = github:Mic92/sops-nix;
+    nixos-hardware.url = github:NixOS/nixos-hardware/master;
+
+    private.url = path:/etc/nixos/nix/private;
+  };
+
+  outputs = { self, nixpkgs, sops-nix, nixos-hardware, private }: {
+
     nixosConfigurations.piserver = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
       modules = [
@@ -12,6 +19,16 @@
         nixos-hardware.nixosModules.raspberry-pi-4
         # Fichier de configuration du système
         ./nix/machines/piserver/configuration.nix
+      ];
+    };
+
+    nixosConfigurations.quark = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        sops-nix.nixosModules.sops
+        private.nixosModules.quark
+        ./nix/machines/quark/configuration.nix
       ];
     };
   };
---
+++ nix/machines/quark/configuration.nix
@@ -0,0 +1,141 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+let
+  hostname = "quark";
+  user = "alventoor";
+
+in {
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ../../modules/base.nix
+      ../../modules/head.nix
+      ../../modules/firefox.nix
+    ];
+
+  sops = {
+    defaultSopsFile = ./secrets.yaml;
+    age.sshKeyPaths = [ "/nix/persist/etc/ssh/ssh_host_ed25519_key" ];
+
+    secrets.alventoor_password = {
+      neededForUsers = true;
+    };
+  };
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    loader.grub = {
+      enable = true;
+      efiSupport = true;
+      useOSProber = true;
+      device = "nodev";
+    };
+  };
+
+  nix.settings.auto-optimise-store = true;
+  nixpkgs.config.allowUnfree = true;
+
+  networking = {
+    hostName = hostname;
+ 
+    # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+    # Per-interface useDHCP will be mandatory in the future, so this generated config
+    # replicates the default behaviour.
+    useDHCP = false;
+    interfaces.enp0s2.useDHCP = true;
+  };
+
+  users = {
+    mutableUsers = false;
+
+    users."${user}" = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.      
+      passwordFile = config.sops.secrets.alventoor_password.path;
+    };
+  };
+
+  environment.etc = {
+    "machine-id".source = "/nix/persist/etc/machine-id";
+
+    "ssh/ssh_host_rsa_key".source = "/nix/persist/etc/ssh/ssh_host_rsa_key";
+    "ssh/ssh_host_rsa_key.pub".source = "/nix/persist/etc/ssh/ssh_host_rsa_key.pub";
+    "ssh/ssh_host_ed25519_key".source = "/nix/persist/etc/ssh/ssh_host_ed25519_key";
+    "ssh/ssh_host_ed25519_key.pub".source = "/nix/persist/etc/ssh/ssh_host_ed25519_key.pub";
+  };
+
+  services = {
+    xserver = {
+      displayManager.sddm.enable = true;
+      desktopManager.plasma5 = {
+        enable = true;
+        excludePackages = with pkgs.libsForQt5; [
+          oxygen
+        ];
+      };
+    };
+
+    fstrim.enable = true;
+  };
+
+  programs = {
+    # Don't work if enabled from environment.systemPackages
+    partition-manager.enable = true;
+
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    helvum
+    wireshark
+    # plasma
+    ark
+    kate
+    skanpage
+    filelight
+    kcalc
+    kcolorchooser
+    # apps
+    mpv
+    qbittorrent
+    teamspeak_client
+    discord
+    krita
+    # libreoffice
+    libreoffice-qt
+    hunspell
+    hunspellDicts.fr-moderne
+    hunspellDicts.en_US
+    # theming
+    papirus-icon-theme
+    # dev
+    git
+    jetbrains.idea-community
+    rustc
+    cargo
+  ];
+
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    hack-font
+  ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+
+}
