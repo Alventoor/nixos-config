23d85baaa336fa262f6661358f4a5359cd16850c
alventoor <julienm99@tutamail.com>
2023-01-24 13:47:30 +0100

    add head module

---
+++ nix/modules/head.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+let keyboard_layout = "fr";
+
+in {
+  security = {
+    polkit.enable = true;
+
+    # Pour pipewire
+    rtkit.enable = true;
+  };
+
+  services.printing = {
+    enable = true;
+    drivers = [ pkgs.hplip ];
+  };
+
+  services.xserver = {
+    enable = true;
+    layout = keyboard_layout;
+  };
+
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+  };
+
+  xdg.portal.enable = true;
+}
